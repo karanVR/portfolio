abf7402d4279fad793b66571f45d34a362373308
karanVR <karanveerraghuvanshi@gmail.com>
2021-11-04 10:25:45 +0530

    #0000
    portfolio-0

---
+++ src/components/About.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BsCode, BsCodeSlash } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className="md:flex justify-between items-center px-10 py-16 text-center items-center">
+      <div className="md:w-1/2 mb-4 lg:mb-0">
+        
+        <h1 className="text-6xl lg:text-7xl text-white font-semibold">
+        <BsCode className='sm:text-3xl text-6xl inline'/> Hi! I am Karan.
+        </h1>
+        <h1 className="sm:text-2xl text-4xl text-white font-semibold mb-6">
+          I am a frontend Developer and I love to build amazing apps <BsCodeSlash className='sm:text-3xl text-6xl inline'/>
+        </h1>
+        <p className="mb-6">
+          I am a software developer specializing in frontend Development, This
+          is my developer portfolio website
+       </p>
+
+        <a
+          className="sm:mb-2 px-4 py-2 mr-4 bg-purple-300 hover:bg-purple-200 rounded text-purple-800 hover:text-purple-900 text-xl transition ease-in duration-150"
+          href="#contact"
+        >
+          Work with me
+        </a>
+
+        <a
+          className="sm:mb-2 px-4 py-2  bg-gray-700 hover:bg-gray-600 rounded text-gray-300 hover:text-gray-50 text-xl transition ease-in duration-150"
+          href="#projects"
+        >
+          See my past work
+        </a>
+      </div>
+
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 items-center">
+        <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src='./IMG_3345.JPG'
+          />
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Private Resort site",
+    subtitle: "Frontend stack",
+    description:
+      "This is a freelance project made for a private resort, worked with google map and razorpay, UI was built with ReactJS",
+    image: "./proj-1.gif",
+    link: "https://theshivalaya.co.in/#/",
+  },
+  {
+    title: "Breaking Bad API",
+    subtitle: "React",
+    description:
+      "Fetched data from Breaking bad show API",
+    image: "./proj-2.gif",
+    link: "https://karanvr.github.io/breaking-bad-react/",
+  },
+  {
+    title: "Task-manager",
+    subtitle: "React",
+    description:
+      "This react app is used to manage tasks and has a functionality to tick or untick pending tasks, built with react, styling done with custom CSS",
+    image: "./proj-3.gif",
+    link: "https://karanvr.github.io/react-task-tracker/",
+  },
+  {
+    title: "Expense Tracker",
+    subtitle: "React Hooks, context",
+    description:
+      "This app keeps track of the money spent and earned and updates all transactions in real time and stores them, developed using Context and React hooks",
+    image: "./proj-4.gif",
+    link: "https://karanvr.github.io/react-expense-tracker/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "We worked on a startup for a while together, and karan used to develop UI screens for web and plan ux, it was amazing working together!",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Ayush dayal",
+    company: "Gamepod",
+  },
+  {
+    quote:
+      "he made the frontend for our resort site, the UI is really clean and minimal, he also did all the changes that we expected from him in given time",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Amitabh solanki",
+    company: "Shivalaya Resort",
+  },
+];
+
+export const skills = [
+  "React",
+  "JavaScript",
+  "React-Redux",
+  "HTML-CSS",
+  "Tailwind CSS",
+  "UI/UX Design",
+];
